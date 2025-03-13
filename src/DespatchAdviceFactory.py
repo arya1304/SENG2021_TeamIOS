d74f0a3748c572cc3cc31448b5e34d6658d03ee2
Arya Thachil <a.thachil@student.unsw.edu.au>
2025-03-13 15:32:33 +1100

    completed order reference, shipment, despatch advice

--- src/DespatchAdviceFactory.py
+++ src/DespatchAdviceFactory.py
@@ -4,47 +4,81 @@ from pydanticModels import models2, models
 from abc import ABC, abstractmethod
 import xml.etree.ElementTree as ET
 import uuid
+from datetime import datetime
 
 class DespatchAdviceFactory :
-    #create each component of the despatch advice
-
     #create order reference
     def create_order_reference(self, orderAdvice: models2.Order) -> models.CacOrderReference:
-        order_dict = orderAdvice.model_dump()
-        lineItem_dict = orderAdvice.cac_OrderLine.cac_LineItem.model_dump()
+        lineItem_dict = orderAdvice.cac_OrderLine.cac_LineItem
         order_ref = models.CacOrderReference(
-            cbc_ID = order_dict['cbc_ID'],
-            cbc_SalesOrderID = lineItem_dict['cbc_SalesOrderID'],
-            cbc_UUID = order_dict['cbc_UUID'],
-            cbc_IssueDate = order_dict['cbc_IssueDate'],
+            cbc_ID = orderAdvice.cbc_ID,
+            cbc_SalesOrderID = lineItem_dict.cbc_SalesOrderID,
+            cbc_UUID = orderAdvice.cbc_UUID,
+            cbc_IssueDate = orderAdvice.cbc_IssueDate,
         )
         return  order_ref
 
     #create despatch supplier party
     #from the order advice use SellerSupplierParty
-    def create_despatch_supplier_party(orderAdvice: models2.Order):
+    def create_despatch_supplier_party(self, orderAdvice: models2.Order) -> models.CacDespatchSupplierParty:
         pass
 
     #create delivery customer party
     #from the order use Buyer customer Party
-    def create_delivery_customer_party(orderAdvice: models2.Order):
+    def create_delivery_customer_party(self, orderAdvice: models2.Order) -> models.CacDeliveryCustomerParty:
         pass
 
     #create shipment
     #extract from the shipment json
-    def create_shipment(shipment: models2.Order):
-        pass
+    #what happens when we get an empty field?
+    #create an optional field for requested delivery period 
+    #determine what fields that can be
+    def create_shipment(self, shipment: models.CacShipment) -> models.CacShipment:
+        despatch_delivery = models.CacShipment(
+            cbc_ID = shipment.cbc_ID,
+            cac_Consignment = shipment.cac_Consignment,
+            cac_Delivery = shipment.cac_Delivery
+        )
+        return despatch_delivery
+
 
     #create despatch line
     #map items, quantities and price details from the order advice
-    def despatch_line(orderAdvice: models2.Order):
-        pass
+    #each item has its own despatch line 
+
+    def create_despatch_line(self, orderAdvice: models2.Order) -> models.CacDespatchLine:
+        despatch_line_dict = orderAdvice.cac_OrderLine
+        line_item_dict = orderAdvice.cac_OrderLine.cac_LineItem
+
+        despatch_line = models.CacDespatchLine(
+
+        )
+        return despatch_line
 
     #create despatch advice
-    def create_despatch_advice(self, orderAdivce: models2.Order, shipment: models.CacShipment) -> models.DespatchAdvice :
+    def create_despatch_advice(self, orderAdivce: models2.Order, shipment: models.CacShipment) -> models.DespatchAdvice:
         #will return a peydantic model of the despatch advice
-        order_ref = self.create_order_reference(orderAdivce) 
-        return 
+        despatch_advice = models.DespatchAdvice(
+            field_xmlns_cbc = orderAdivce.field_xmlns,
+            field_xmlns_cac = orderAdivce.field_xmlns_cac,
+            field_xmlns = orderAdivce.field_xmlns,
+            cbc_UBLVersionID = orderAdivce.cbc_UBLVersionID,
+            cbc_ProfileID = orderAdivce.cbc_ProfileID,
+            cbc_ID = '',
+            cbc_CopyIndicator = 'false',
+            cbc_UUID = uuid.uuid4(), #generating a uuid
+            cbc_IssueDate = datetime.today().strftime('%Y-%m-%d'),
+            cbc_DocumentStatusCode = 'NoStatus',
+            cbc_DespatchAdviceTypeCode = 'delivery',
+            cbc_Note = '',
+            order_ref = self.create_order_reference(orderAdivce), 
+            supplier_party = self.create_despatch_supplier_party(orderAdivce),
+            customer_party = self.create_delivery_customer_party(orderAdivce),
+            shipment_details = self.create_shipment(shipment),
+            despatch_line = self.create_despatch_line(orderAdivce)
+        )
+        return despatch_advice
+    
     #convert it into xml format
 
     #https://docs.oasis-open.org/ubl/os-UBL-2.4/xml/UBL-DespatchAdvice-2.0-Example.xml 
