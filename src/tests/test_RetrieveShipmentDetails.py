26a4b2128a1086f18afc915048483435ffcff952
Jimin <jimin.park@student.unsw.edu.au>
2025-03-17 11:10:17 +1100

    test for retrieveShipmentDetails.py

--- src/tests/test_RetrieveShipmentDetails.py
+++ src/tests/test_RetrieveShipmentDetails.py
@@ -21,7 +21,7 @@ def test_retrieve_success(mock_boto_resource, mock_dynamodb):
     mock_table.get_item.return_value = {
         "Item": {
             "ID": "123",
-            "Items": "shipmentDetails"
+            "ShipmentDetails": "shipmentDetails"
         }
     }
 
@@ -44,4 +44,17 @@ def test_invalid_Id(mock_boto_resource, mock_dynamodb):
 
     assert response["statusCode"] == 404
     mock_table.get_item.assert_called_once_with(Key={"ID": "notexist"})
+    mock_table.delete_item.assert_not_called()
+
+@patch("boto3.resource")
+def test_empty_parameters(mock_boto_resource, mock_dynamodb):
+    mock_resource, mock_table = mock_dynamodb
+    mock_boto_resource.return_value = mock_resource
+
+    mock_table.get_item.return_value = {}
+
+    event = {"pathParameters": ""}
+    response = lambda_handler(event, {})
+
+    assert response["statusCode"] == 400
     mock_table.delete_item.assert_not_called()
