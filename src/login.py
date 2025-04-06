1671e98d518aeb3e7567f45cf57532b69dc3e0bd
Arya Thachil <a.thachil@student.unsw.edu.au>
2025-04-06 20:38:33 +1000

    authentication fixed bugs

--- src/login.py
+++ src/login.py
@@ -1,46 +1,37 @@
 import boto3
-from datetime import datetime,timedelta
 import jwt
 
 SECRET_KEY = "hello"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-def createtoken(username:str):
-    expire = datetime.now()+timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    payload = {
-        "username": username,
-        "expire": expire
-    }
-    encoded_token = jwt.encode(payload,SECRET_KEY,algorithm=ALGORITHM)
-    return encoded_token
 
 def lambda_handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('DespacthAdviceUsers') 
-    username = event['pathParameters']['username']
-    password = event['pathParameters']['password']
-
-    response = table.get_item(Key={'username' : username})
-    if 'Item' not in response:
-        return {
-            "statusCode": 404,
-            "body": "This user does not exist"
-        }
-
-    #check if the username and password is correct
-    user_details = response['Item']
-    password_from_database = user_details.get("password")
-    if password_from_database != password:
-        return {
-            "statusCode": 404,
-            "body": "You have entered an incorrect password"
-        }
+    encoded_token = event['authorizationToken'] 
+    #this decode and verifies our token
+    try:
+        jwt.decode(jwt=encoded_token, key=SECRET_KEY, algorithms=["HS256"])
+        auth_status = "Allow"
+    except jwt.ExpiredSignatureError:
+        auth_status = "Deny"
+        print("Expired token1")
+    except jwt.InvalidTokenError:
+        auth_status="Deny"
+        print("Expired token2")
+    except jwt.InvalidSignatureError:
+        auth_status="Deny"
+        print("Expired token3")
     
-    #generate a jwt token and store it in the username database
-    token = createtoken(username)
-
-    return {
-        "statusCode": 200,
-        "token" : token
+    authResponse = {
+        'policyDocument': { 
+            'Version': '2012-10-17',
+            'Statement': [
+                {
+                    'Action': 'execute-api:Invoke',
+                    'Resource': [
+                        event['methodArn']
+                    ], 
+                    'Effect': auth_status
+                }
+            ]
+        }
     }
+    return authResponse
--- src/validateToken.py
+++ src/validateToken.py
@@ -4,20 +4,27 @@ import jwt
 SECRET_KEY = "hello"
 ALGORITHM = "HS256"
 
+
+# https://www.youtube.com/watch?v=5kI2wZWp4NU&ab_channel=RahulNath
+
 def lambda_handler(event, context):
     encoded_token = event['authorizationToken'] 
     #this decode and verifies our token
     try:
-        jwt.decode(jwt=encoded_token, key=SECRET_KEY, algorithms=["HS512"])
+        decoded = jwt.decode(jwt=encoded_token, key=SECRET_KEY, algorithms=["HS256"])
         auth_status = "Allow"
     except jwt.ExpiredSignatureError:
         auth_status = "Deny"
+        print("Expired token1")
     except jwt.InvalidTokenError:
         auth_status="Deny"
+        print("Expired token2")
     except jwt.InvalidSignatureError:
         auth_status="Deny"
+        print("Expired token3")
     
     authResponse = {
+        'principalId': decoded['username'],
         'policyDocument': { 
             'Version': '2012-10-17',
             'Statement': [
@@ -29,6 +36,9 @@ def lambda_handler(event, context):
                     'Effect': auth_status
                 }
             ]
+        },
+        "context":{
+            "username": decoded['username']
         }
     }
     return authResponse
